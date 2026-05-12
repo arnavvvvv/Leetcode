16e556f49fb81859686a3e78e83547461b28eefe
arnavvvvv <65419893+arnavvvvv@users.noreply.github.com>
2026-05-11 23:43:34 -0500

    Time: 16 ms (95%), Space: 23.4 MB (90.83%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,7 +15,6 @@ public:
             return lowestCommonAncestor(root->right, p, q);
         else if(root->val > p->val && root->val > q->val)
             return lowestCommonAncestor(root->left, p, q);
-    
         return root;
     }
 };
