031a4980a2d90b5412102b1f8d4c2d94c583266c
arnavvvvv <65419893+arnavvvvv@users.noreply.github.com>
2026-05-14 01:24:25 -0500

    Time: 26 ms (2.72%), Space: 8.8 MB (7.91%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,37 @@
+typedef pair<char, int> ci;
+struct Comparator{
+    bool operator()(const ci& lhs, const ci& rhs) {
+        return lhs.second < rhs.second;
+    }
+};
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> m;
+        char temp = ' ';
+        int n = s.size();
+        string ans = "";
+        for(char x: s)
+            ++m[x];
+        priority_queue<ci, vector<ci>, Comparator> max_heap(begin(m), end(m));
+        while(n && max_heap.size()) {
+            cout<<max_heap.top().first<<" "<<max_heap.top().second<<endl;
+            char x = max_heap.top().first;
+            max_heap.pop();
+            ans += x;
+            --m[x];
+            if(temp != ' ' && m[temp])
+                max_heap.push(ci(temp, m[temp]));
+            temp = x;
+            --n;
+
+        }
+        if(n == 0)
+            return ans;
+        return "";
+
+
+
+    }
+};
