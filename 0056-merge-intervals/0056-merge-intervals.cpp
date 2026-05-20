6c73d147e821a64693143b74fb98f949e6d03f43
arnavvvvv <65419893+arnavvvvv@users.noreply.github.com>
2026-05-20 12:24:25 -0500

    Time: 103 ms (5.13%), Space: 24.8 MB (16.88%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,31 @@
+bool smaller_interval(vector<int>& lhs, vector<int>& rhs) {
+    if(lhs[0] < rhs[0])
+        return true;
+    if(lhs[0] == rhs[0])
+        return lhs[1] < rhs[1];
+    return false;
+}
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(begin(intervals), end(intervals), smaller_interval);
+        for(auto x: intervals)
+            cout<<x[0]<<","<<x[1]<<"   ";
+        vector<vector<int>> ans;
+        for(int i = 0; i < intervals.size(); ++i) {
+            int j = i;
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            while(j < intervals.size() - 1 && intervals[j+1][0] <= end) {
+                if(intervals[j + 1][1] > end)
+                    end = intervals[j + 1][1];
+                ++j;
+            }
+            ans.push_back({start, end});
+            i = j;
+               
+        }
+        return ans;
+        
+    }
+};
