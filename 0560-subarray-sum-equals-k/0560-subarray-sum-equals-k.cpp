7fe97b64b217731815158d9b50f92c68e3337609
arnavvvvv <65419893+arnavvvvv@users.noreply.github.com>
2026-05-22 00:47:52 -0500

    Time: 497 ms (20.01%), Space: 48.4 MB (21.94%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        vector<int> prefix_sum;
+        int count = 0;
+    
+        prefix_sum.push_back(nums[0]);
+        for(int& x: nums | std::views::drop(1)) {
+            prefix_sum.push_back(x + prefix_sum.back());
+        }
+        for(int& x: prefix_sum) {
+            cout<<x<<endl;
+            if(m.find(x - k) != m.end())
+                count += m[x - k];
+            if(x == k)
+                ++count;
+            ++m[x];
+        }
+        return count;
+
+    }
+};
