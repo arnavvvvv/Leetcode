f66845ffe16d5359a5bf293a448fa6154f0bceba
arnavvvvv <65419893+arnavvvvv@users.noreply.github.com>
2026-06-29 02:05:02 -0500

    Time: 1 ms (49.53%), Space: 40.2 MB (60.76%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<int> suffix(n + 1);
+        // vector<int> prefix(n + 1);
+        // vector<int> ans(n);
+        // prefix[0] = 1;
+        // suffix[n] = 1;
+        // for(int i = 0; i < nums.size(); ++i) {
+        //     prefix[i + 1] = prefix[i] * nums[i];
+        //     suffix[n - i - 1] = suffix[n - i] * nums[n - i - 1];
+        // }
+        // for(int i = 0; i < nums.size(); ++i) {
+        //     ans[i] = prefix[i] * suffix[i + 1];
+        // }
+        // return ans;
         int n = nums.size();
-        vector<int> suffix(n + 1);
-        vector<int> prefix(n + 1);
-        vector<int> ans(n);
-        prefix[0] = 1;
-        suffix[n] = 1;
-        for(int i = 0; i < nums.size(); ++i) {
-            prefix[i + 1] = prefix[i] * nums[i];
-            suffix[n - i - 1] = suffix[n - i] * nums[n - i - 1];
+        vector<int> res(n);
+        int prod = 1;
+        res[0] = prod;
+        for(int i = 0; i < n - 1; ++i) {
+            prod = prod * nums[i];
+            res[i + 1] = prod;
         }
-        for(int i = 0; i < nums.size(); ++i) {
-            ans[i] = prefix[i] * suffix[i + 1];
+        prod = 1;
+        for(int i = n - 1; i > 0; --i) {
+            prod = prod * nums[i];
+            res[i - 1] *= prod;
         }
-        return ans;
+        return res;
+        
 
         
     }
